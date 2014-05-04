4a65481ae0826522cf5760dd5ed02ae91a39ab89
Ivan Alves da Silva <ivan.novo@hotmail.com>
2014-05-04 02:13:05 -0300

    view inicial
    Arquivos iniciais html

---
+++ view/forms/cadastrar_usuario.php
@@ -0,0 +1,35 @@
+<section class="container-fluid">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h3>Cadastrar novo usuário</h3>
+        </div>
+        <div class="panel-body"> 
+            <form class="form-horizontal" role="form" action="#">
+                <div class="form-group">
+                    <label for="emailNovoUsuario" class="col-sm-2 control-label">Email</label>
+                    <div class="col-sm-6">
+                        <input type="email" class="form-control" id="emailNovoUsuario" title="Insira o email da pessoa que deseja cadastrar" placeholder="Email">
+                    </div>
+                </div>
+                
+                <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-10">
+                        <button id="btn-inicio-novo-usuario" class="btn btn-default">Cadastrar</button>
+                    </div>
+                </div>
+            </form>
+
+
+        </div>
+        <div class="panel-footer">
+            <p class="text-muted"> Será enviado um link para o email cadastrado com as instruções complementares.</p>
+        </div>
+    </div>
+
+</section>
+
+<script type="text/javascript">
+   Apanhando aqui
+   
+   
+</script>    
